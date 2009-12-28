61670b8b1910928b6dea0d55d4708f73604c0499
Jason Wang <randomtalk@gmail.com>
2009-12-27 21:09:15 -0500

    moving computers, deleted the old code, and reformatted the code

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 PORT = 7000
 HOST = null;
 
-var awe = require('./blog');
+var b = require('./blog');
 var sys = require('sys');
 
-awe.listen(PORT, HOST);
+b.listen(PORT, HOST);
 
-awe.get('/', awe.sendFile('index.html'));
+b.get('/', b.sendHTML(process.platform));
 
